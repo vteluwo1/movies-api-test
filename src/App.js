d3c8a9f48388320d509632b083c7477eb7004830
Victor A. Teluwo <vateluw1@hobbylobby.corp>
2023-06-29 16:29:39 -0500

    home ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import api from './api/axiosConfig'
+import Layout from './components/layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+
+
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  async function getMovies() {
+    try {
+      const response = await api.get("/api/v1/movies")
+      console.log(response.data);
+      setMovies(response.data)
+    
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        <Route path='/' element={<Layout />}>
+
+        <Route path='/' element={<Home movies={movies}/>} > </Route>
+
+        </Route>
+      </Routes>
+     
     </div>
   );
 }
 
 export default App;
+
